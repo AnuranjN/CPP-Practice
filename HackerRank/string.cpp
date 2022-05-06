5712247471b5e9330ae333552b7c0ee1dabe17f9
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-06 23:30:34 +0530

    Create string.cpp

---
+++ HackerRank/string.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    string str1,str2,str3;
+    char b,a;
+    int strlen1,strlen2;
+    cin>>str1;
+    cin>>str2;
+    
+    strlen1 = str1.size();
+    strlen2 = str2.size();
+    cout<<strlen1;
+    cout<<" "<<strlen2;
+    
+    // Now concatenate
+    str3 = str1+str2;
+    cout<<"\n"<<str3;
+    
+    // a' b'
+    a = str2[0];
+    b = str1[0];        
+    str1[0] = a;
+    str2[0] = b;
+    cout<<"\n";
+    cout<<str1;
+    cout<<" "<<str2;
+    return 0;
+}
