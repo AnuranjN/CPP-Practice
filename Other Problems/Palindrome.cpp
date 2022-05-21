08fc116e57489b2745348bad08d4023b6d13d030
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-21 07:35:28 +0530

    Create Palindrome.cpp

---
+++ Other Problems/Palindrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main(){                                  
+    int n,r,rev=0;
+    cin>>n;
+    int m=n;
+    while(n>0){                             
+        r=n%10;
+        n=n/10;
+        rev=rev*10+r;
+        
+    }
+    cout<<rev<<std::endl;;
+    if(rev==m){                                  
+        cout<<"Its a palindrome"<<std::endl;
+    }
+    else{
+        cout<<"Not a palindrome"<<std::endl;
+    }
+    return 0;
+}    
