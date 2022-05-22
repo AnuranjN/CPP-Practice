9a45c1cb9290148581e9da4bc7bcac5da4c05ad0
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-22 22:53:10 +0530

    Update Square.cpp

--- Other Problems/Patterns/Square.cpp
+++ Other Problems/Patterns/Square.cpp
@@ -7,7 +7,7 @@ int main(){
     std::cout<<"Enter number";
     std::cin>>n;
     
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++){                          //Here i is row and j is column
         for(int j=0;j<n; j++){
             std::cout<<"* ";
         }
