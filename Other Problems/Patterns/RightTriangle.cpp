80c93e4a4543bd5ef16980210d0a4a418c58e44c
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-24 22:15:53 +0530

    Create RightTriangle.cpp

---
+++ Other Problems/Patterns/RightTriangle.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using std::cin;
+using std::cout;
+
+int main(){
+    int n;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i>=j){
+                cout<<"* ";
+                
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<std::endl;
+    }
+    
+}
