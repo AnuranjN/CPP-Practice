c546e94e7a86653709ee9a0875b80dd134761ccb
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-28 18:52:05 +0530

    Update string_length.cpp

--- Other Problems/string_length.cpp
+++ Other Problems/string_length.cpp
@@ -3,10 +3,10 @@
 
 int main(){
     std::string str;
-    std::cin>>str;
+    std::cin>>str;                                       //input string
     
-    int count=0;
-    for(int i=0;str[i]!='\0';i++){
+    int count=0;                                         //taking a counter
+    for(int i=0;str[i]!='\0';i++){                       //Here we run for loop till it reaches null
         count ++;
     }
     std::cout<<"Length is: "<<count<<std::endl;
