2e62253659049c55c8a079f142ef59d073c6d7bc
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-28 18:56:19 +0530

    Create string_case.cpp

---
+++ Other Problems/string_case.cpp
@@ -0,0 +1,13 @@
+//Program to change cases of a letter(UPPER TO LOWER)
+#include<iostream>
+#include<string>
+
+int main(){                                        //taking string input
+    std::string str;
+    std::cin>>str;
+
+    for(int i=0; str[i]!='\0';i++){
+        str[i]+=32;                               //here 32 is the difference between ASCI values of uppercase and lowercase letters
+    }
+    std::cout<<str<<std::endl;
+}
