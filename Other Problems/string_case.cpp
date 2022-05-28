77c3c9c6454ce56f96edf9f9b1e1e795f5929d16
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-28 18:58:13 +0530

    Update string_case.cpp

--- Other Problems/string_case.cpp
+++ Other Problems/string_case.cpp
@@ -7,7 +7,7 @@ int main(){                                        //taking string input
     std::cin>>str;
 
     for(int i=0; str[i]!='\0';i++){
-        str[i]+=32;                               //here 32 is the difference between ASCI values of uppercase and lowercase letters
+        str[i]+=32;                               //here 32 is the difference between ASCII values of uppercase and lowercase letters
     }
     std::cout<<str<<std::endl;
 }
