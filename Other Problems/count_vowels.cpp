6823c0601509bb6bdd93b3ccd7c262820600727f
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-29 11:39:22 +0530

    Update count_vowels.cpp

--- Other Problems/count_vowels.cpp
+++ Other Problems/count_vowels.cpp
@@ -1,12 +1,13 @@
+//Program to count number of vowels
 #include<iostream>
 #include<string.h>
 
 
 int main(){
-    std::string str;
-    getline(std::cin,str);
+    std::string str;              //declaring a string called str
+    getline(std::cin,str);        //We use getline function to read multiple words from the string
     
-    int vowels=0;
+    int vowels=0;                 //declaring a counter called vowels
     
     for(int i=0; str[i]!='\0'; i++){
         if(str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' ||
