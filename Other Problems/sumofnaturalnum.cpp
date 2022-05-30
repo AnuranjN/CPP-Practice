fb3746c139fae1b2646b9ab54d1119c070abe63c
Anuranj N <100217096+AnuranjN@users.noreply.github.com>
2022-05-30 19:25:02 +0530

    Update sumofnaturalnum.cpp

--- Other Problems/sumofnaturalnum.cpp
+++ Other Problems/sumofnaturalnum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    int n,sum=0;                   //declaring variable  n and taking input from user
+    int n,sum=0;                   //declaring variable n and taking input from user
     std::cin>>n;
     
     for(int i=1;i<=n;i++){
